43a312690bf5bb2b3ce7c64fdb1ba570eaa94aab
txtp <txtp@txtp.com>
2024-12-30 16:33:58 +0800

    inti

--- src/settings.7307d.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",resources:"340f5",main:"c81b9"}};
